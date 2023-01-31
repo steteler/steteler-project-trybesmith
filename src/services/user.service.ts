1978f7fe4b925a1f96d70e06375324ea2a4cec13
kikuti <joaovictorkikuti@gmail.com>
2023-01-31 02:16:23 -0300

    fix: returning undefined

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -9,7 +9,7 @@ async function getUserByName(user: User): Promise<Response> {
     return { type: 401, message: 'Username or password invalid' };
   }
 
-  return { type: 201, message: getToken(userFound.id as number, userFound) };
+  return { type: null, message: getToken(userFound.id as number, userFound) };
 }
 
 async function createUser(user: User): Promise<Response> {
